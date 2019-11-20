a8420e6746d9f2bf8179d3388914cc02a05991f0
Reem H <reemhamoui92@gmail.com>
2019-11-20 17:49:53 -0500

    started ajax call

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Create app namespace to hold all methods
 const app = {};
-app.URL = 'https://www.anapioficeandfire.com/api/houses/';
+app.URL = 'https://www.anapioficeandfire.com/api/houses/378';
 
 
 
@@ -13,25 +13,32 @@ app.collectInfo = function () {
 //houseID is a parameter that will change based on the div clicked on the interactive map. The div will trigger the ID of the house found in the API. 
 //Once triggered, and will get us the information we want to append to our website. 
 
-app.getInfo = function (houseID) {
-    $.ajax({
-        url: `${app.URL}houseID`,
+app.getInfo = function () {
+    return $.ajax({
+        url: `${app.URL}`,
         method: 'GET',
-        dataType: 'json'
+        dataType: 'json',
     }).then(function (result) {
-        app.displayInfo(result)
+        // app.displayInfo(result)
         console.log(result)
     })
 }
 
 // Display data on the page
 //we will be displaying information gathered from the API data, we will also be appending them to the page
-app.displayInfo = function () {}
+app.displayInfo =  (houseInfo) => {
+    houseInfo.forEach((houseObject) => {
+        // $('.houseShit')
+            // .append(`<h2>${houseObject.name}</h2>`);
+        console.log(houseObject.name);
+    })
+}
 
 // Start app
 //put all the functions we need in here
 app.init = function () {
     app.getInfo();
+    app.displayInfo;
 }
 
 $(function () {
